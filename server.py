6ed2496978262992e0a2e88c5f121f3a88cf0794
kiyan631 <80814660+kiyan631@users.noreply.github.com>
2024-09-14 13:51:49 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,61 @@
+import socket
+from threading import Thread
+import time
+import os
+IP_ADRESS = '127.0.0.2'
+PORT = 8079
+SERVER = None
+clients = {}
+
+
+is_dir_exists = os.path.isdir('shared_files')
+print( is_dir_exists)
+if(not is_dir_exists):
+    os.makedirs('shared_files')
+
+def acceptConnections():
+    global SERVER
+    global clients
+
+    while True:
+        client,addr = SERVER.accept()
+        
+        client_name = client.recv(4096).decode().lower()
+        clients[client_name] = {
+                "client" : client,
+                "address" : addr,
+                "connected_with" : "",
+                "file_name" : "",
+                "file_size" : 4096
+            }
+
+        print(f"Connection established with {client_name}:{addr}")
+
+        thread = Thread(target=handleClient, args=(client,client_name,))
+        thread.start()
+
+
+def handleClient():
+    pass
+    
+
+
+def setup():
+    print("\n\t\t\t\t\t\t MUSICAL APP\n")
+
+
+    global PORT
+    global IP_ADRESS
+    global SERVER
+
+    SERVER = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    SERVER.bind((IP_ADRESS,PORT))
+    SERVER.listen(100)
+
+    print("\t\t\tSERVER IS WAITING FOR INCOMING CONNECTIONS...")
+    print("\n")
+
+    acceptConnections()
+
+setup_thread = Thread(target=setup)
+setup_thread.start()
